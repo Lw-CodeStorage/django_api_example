aa9d2f243ce69515619d1d3397641cec48b6e109
Leo <adamwang1209@gmail.com>
2024-11-18 01:53:34 +0800

    更換專案名稱

--- django_api_example/urls.py
+++ django_api_example/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_session project.
+URL configuration for django_api_example project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
